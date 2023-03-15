e44cc6bdcdb6908023d835b80f8242a203771260
temujin469 <ttemujin469@gmail.com>
2023-03-15 22:00:38 +0800

    add modal

---
+++ js/modal.js
@@ -0,0 +1,54 @@
+// var span = document.getElementsByClassName("close");
+
+// function openModal(id) {
+//   var modal = document.getElementById(id);
+
+//   modal.style.display = "block";
+
+//   // span.forEach((element) => {
+//   //   element.onclick = function () {
+//   //     modal.style.display = "none";
+//   //   };
+//   // });
+
+//
+// }
+
+// for (i = 0; i < span.length; i++) {
+//   span[i].onclick = function () {
+//     modal.style.display = "none";
+//   };
+// }
+
+document.addEventListener(
+  "click",
+  function (e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
+
+    // if (
+    //   target.hasAttribute("data-toggle") &&
+    //   target.getAttribute("data-toggle") == "modal"
+    // ) {
+    if (target.hasAttribute("data-target")) {
+      var m_ID = target.getAttribute("data-target");
+      document.getElementById(m_ID).style.display = "block";
+      document.getElementById(m_ID).classList.add("open");
+      e.preventDefault();
+    }
+    // }
+
+    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+    if (
+      (target.hasAttribute("data-dismiss") &&
+        target.getAttribute("data-dismiss") == "modal") ||
+      target.classList.contains("modal")
+    ) {
+      var modal = document.querySelector('[class="modal open"]');
+      modal.style.display = "none";
+      modal.classList.remove("open");
+      e.preventDefault();
+    }
+  },
+  false
+);
